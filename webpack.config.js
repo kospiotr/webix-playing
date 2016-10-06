df7d98688ec961ce448d5b398ac5604a42d85ced
Piotr Kosmowski <kospiotr@gmail.com>
2016-10-06 14:13:10 +0200

    init webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+  entry: {
+    bootstrap: './src/bootstrap.js'
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].js',
+    chunkFilename: '[name].[chunkhash].js'
+  },
+  module: {
+    loaders: [
+      {test: /\.css$/, loader: 'style!css'},
+      {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff"},
+      {test: /\.(ttf|eot|svg|png|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"},
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['build'], {
+      verbose: true,
+      dry: false
+    }),
+    new CopyWebpackPlugin([
+      {context: path.join(__dirname, 'src'), from: '**/*.html'}
+    ], {}),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 4000,
+      server: {baseDir: ['build']}
+    })
+  ]
+
+};
